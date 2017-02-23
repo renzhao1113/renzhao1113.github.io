fdca159059be0d16cbc9f21dd580502fbd08b0b5
Nicolas <547249523@qq.com>
2017-02-23 13:10:14 +0800

    产品服务修改完毕

--- js/imgs.js
+++ js/imgs.js
@@ -27,7 +27,6 @@ var imgs = [
   "img/products/product_27.png"
 ];
 var oilImgs = [
-
 	"img/products/product_02.png",
 	"img/products/product_03.png",
 	"img/products/product_04.png",
@@ -35,9 +34,6 @@ var oilImgs = [
 	"img/products/product_06.png",
 ];
 var maintainImgs = [
-	"img/products/product_05.png",
-	"img/products/product_06.png",
-	"img/products/product_07.png",
 	"img/products/product_08.png",
 	"img/products/product_09.png",
 	"img/products/product_10.png",
@@ -46,14 +42,16 @@ var maintainImgs = [
 	"img/products/product_13.png",
 	"img/products/product_14.png",
 	"img/products/product_15.png",
-	"img/products/product_16.png"
-];
-var consumablesImgs = [
+	"img/products/product_16.png",
 	"img/products/product_18.png",
 	"img/products/product_19.png",
-	"img/products/product_20.png",
+];
+var consumablesImgs = [
 	"img/products/product_21.png",
 	"img/products/product_22.png",
 	"img/products/product_23.png",
-	"img/products/product_24.png"
+	"img/products/product_24.png",
+	"img/products/product_25.png",
+	"img/products/product_26.png",
+	"img/products/product_27.png"
 ]
